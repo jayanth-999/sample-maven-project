9e807d4a8bc5df237872d56550600fb5f5dfa462
jayanth-999 <jayanthnuggu@gmail.com>
2024-02-28 15:27:50 +0530

    inital proj

---
+++ src/test/java/com/example/live/LiveApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.live;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import java.io.ByteArrayOutputStream;
+// import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+	// @Test
+	// public void testMain() {
+	// 	ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	// 	System.setOut(new PrintStream(outContent));
+
+	// 	LiveApplication.main(new String[] {});
+
+	// 	assertEquals("Hello World!\n", outContent.toString());
+
+	// 	System.setOut(System.out);
+	// }
+}
